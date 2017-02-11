f6ddbfb22fa5fd71a57665c8b3b6fc80ed6318ca
telsa <telsaiori.git@gmail.com>
2017-02-11 17:31:17 +0800

    init

---
+++ sk.js
@@ -0,0 +1,58 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var resetButton = document.querySelector("#reset");
+var p1Score = 0;
+var p2Score = 0;
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+var gameOver = false;
+var winningScore = 5;
+var winningScoreDisplay = document.querySelector("p span");
+var numInput = document.querySelector("input");
+
+winningScoreDisplay.textContent = winningScore;
+
+p1Button.addEventListener("click", function(){
+  if (!gameOver) {
+    p1Score++;
+    if (p1Score == winningScore){
+      gameOver = true;
+      p1Display.classList.add("winner");
+
+    }
+  }
+  console.log(p1Score);
+  p1Display.textContent = p1Score;
+});
+
+p2Button.addEventListener("click", function(){
+  if (!gameOver){
+    p2Score++;
+    if (p2Score == winningScore){
+      p2Display.classList.add("winner");
+      gameOver = true;
+    }
+  }
+  p2Display.textContent = p2Score;
+});
+
+function reset(){
+  p1Score = 0;
+  p2Score = 0;
+  gameOver = false;
+  winningScore = 5;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+}
+
+numInput.addEventListener("change", function(){
+  winningScoreDisplay.textContent = this.value;
+  winningScore = this.value;
+  reset();
+})
+
+
+resetButton.addEventListener("click", reset);
+
